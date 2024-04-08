3caba32a40cdcc4681ec0bcd44777fb45d8792c1
Van Thao <levanthao@gmail.com>
2024-04-08 15:54:22 +0700

    finish

---
+++ ecommerce/resources/views/layouts/app.blade.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VAN THAO SHOP</title>
+      <!-- CSS Bootstrap 5 -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- JavaScript Bootstrap 5 (tùy chọn, nếu bạn cần sử dụng các tính năng tương tác như modal, collapse, dropdown...) -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+      {{-- link font awesome --}}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+      .gap-img{
+        margin-right: 1.3%;
+      }
+      .font-small{
+        font-size: 10px;
+      }
+    </style>
+
+    
+</head>
+<body>
+    <div class="container-fluid bg-light">
+        <div class="row">
+            <div class="col-sm-1" style="background-color: #ffffff">
+                <ul class="list-group">
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-television fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                    <li class="list-group-item" style="border: none;"><i class="fa fa-list-ul fa-2x" aria-hidden="true"></i></li>
+                  </ul>
+            </div>
+
+            <div class="col-sm-11 my-0">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <nav class="navbar bg-body-tertiary">
+                        <div class="container">
+                          <a class="navbar-brand" href="#">
+                            <img src="{{asset('images/parrot.png')}}" alt="Bootstrap" width="100" height="89">
+                          </a>
+                        </div>
+                      </nav>
+                    <div class="container-fluid">
+                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      
+                        <li class="nav-item" style="margin-left: 196px;">
+                          <form action="" class="" style="align-item: center">
+                            <input class="rounded-pill px-4 " type="text" style="width:40rem" placeholder="Search product">
+                            
+                          </form>
+                        </li>
+
+                        
+
+                        
+                          <li class=""  style="padding-left: 47px;">
+                            
+                            <img src="{{asset('images/avt.jpg')}}" class="rounded-circle" alt="..." width="60%">
+                          
+                          </li>
+                          <li class=" dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Diego Morata
+                            </a>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">Action</a></li>
+                              <li><a class="dropdown-item" href="#">Another action</a></li>
+                              <li><hr class="dropdown-divider"></li>
+                              <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Chart</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Order</a>
+                          </li>
+                        </ul>
+                      
+                    </div>
+                  </nav>
+              <hr>
+              <div class="row mt-4">
+                <div class="col-sm-3">
+                  <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Traditional Wear
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Western Wear
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Swim and Beachwear
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Winter and Seasonal Wear
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Beauty and groomng
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Juwellery
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Persional care appliances
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      International Brands
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Foot Wear
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Watches
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Accessories
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center p-2">
+                      Check
+                      <span class="badge text-bg-light rounded-pill" ><i class="fa fa-caret-right fa-2" aria-hidden="true"></i></span>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-sm-6">
+                  <div class="row">
+                    <div class="text-center">
+                      <img src="{{asset('images/chair1.png')}}" class="img-fluid" alt="..." width="100%" height="auto">
+                    </div>  
+                  </div>
+                  <div class="row mt-2">
+                    <div style="display:inline-block">
+                        <img src="{{asset('images/chair.jpg')}}" class="img-thumbnail gap-img" alt="..." width="32%" height="auto">                  
+                        <img src="{{asset('images/chair.jpg')}}" class="img-thumbnail gap-img" alt="..." width="32%" height="auto">                      
+                        <img src="{{asset('images/chair.jpg')}}" class="img-thumbnail " alt="..." width="32%" height="auto">                     
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-sm-3 ml-3">
+                  
+                      <p><b>Recently viewed</b></p>
+                      <div>
+                        <img src="{{asset('images/handbag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      </div>
+                      <div class="card-body">
+                        <p class="card-text font-small"><b>See you browsing history</b></p>
+                      </div>
+
+                    <p><b>Suggest for you</b></p>
+                    <div>
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                    </div>
+                    <div class="card-body">
+                      <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                    </div>
+                  </div>
+                
+                </div>
+
+                <hr>
+                <div class="row d-flex">
+
+                  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                      <a class="navbar-brand" href="#"><b>Deal of the day <span class="text-danger m-2"> |  20:45:12 left</span></b></a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#"></a>
+                          </li>
+                        </ul>
+                        <span class="navbar-text">
+                          View All Deals
+                        </span>
+                      </div>
+                    </div>
+                  </nav>
+
+
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                    <div class="col-sm-2">
+                      <img src="{{asset('images/bag.jpg')}}" class="" alt="..." width="60%" height="90%">
+                      <div class="card-body">
+                        <p class="card-text font-small bg-light"><b>Watch more</b></p>
+                      </div>
+                    </div>
+                </div>
+              </div>                                                    
+        </div>
+    </div>
+</body>
+</html>
